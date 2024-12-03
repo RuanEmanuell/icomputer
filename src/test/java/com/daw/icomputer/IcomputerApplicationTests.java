8ee0bb5eda55784e16396e09dcfbb8e4c794240a
ruanemanuell <ruanvitorcom540@Gmail.com>
2024-12-03 20:46:00 -0300

    feat: starting the project

---
+++ src/test/java/com/daw/icomputer/IcomputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.icomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcomputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
