a752b13ba710ae95c837f9112f555718fa09e7e8
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-12-13 19:48:16 -0300

    feat: adding first classes

--- src/main/java/com/daw/icomputer/IcomputerApplication.java
+++ src/main/java/com/daw/icomputer/IcomputerApplication.java
@@ -2,15 +2,16 @@ package com.daw.icomputer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@EnableJpaRepositories("com.daw.icomputer.repository")  
+@EntityScan("com.daw.icomputer.model") 
 public class IcomputerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IcomputerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(IcomputerApplication.class, args);
+    }
 }
